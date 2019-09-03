e4b888bf236b6b4e916c5dd7d81151d74b1f619a
mtzainab <preetyzee9911@gmail.com>
2019-09-03 10:21:36 -0600

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,49 @@
+/*function checkmin(a,b){
+    if (a < b){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+let a= prompt("enter first number","0")
+let b = prompt("enter second number","0")
+if (checkMin(a,b)){
+    console.log(a + " is the min number")
+}else{
+    console.log(b + " is the min number")
+
+}*/
+
+
+
+
+/*LOOP
+for (i=0; i < 10; i++){
+    console.log("try" + i)
+}*/
+  
+
+ 
+
+
+/*ARRAYS
+ let countries = ["NIGERIA", "GHANA", "TOGO", "CONGO"];
+ console.log(countries);
+ let newCountry = prompt("Enter a new COUNTRY")
+ countries.push(newCountry);
+ console.log(countries);
+ */
+            
+ 
+ 
+ 
+ /*OBJECT*/
+            let countries={"NIGERIA" : "ABUJA", "GHANA":"ACCRA", "KENYA":"NAIROBI", "CONGO":"CONGO"};
+
+        console.log(countries);
+        let newCountry= prompt("enter a new country")
+        let element = ""
+        countries.push(element);
+        console.log(countries);
+        console.log(countries[2]);
